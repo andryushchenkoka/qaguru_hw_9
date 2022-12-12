d666223bddce4da3fa93f479b41263d72b0490d6
KirillAndryushchenko <kirill2goo@gmail.com>
2022-12-12 13:30:58 +0300

    Добавлены lambda-steps

--- src/test/java/tests/GithubTests.java
+++ src/test/java/tests/GithubTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 import static org.openqa.selenium.By.linkText;
 
 public class GithubTests extends BaseTest {
@@ -17,9 +19,12 @@ public class GithubTests extends BaseTest {
             REPOSITORY_PATH = "andryushchenkoka/qaguru_hw_9",
             ISSUE_TEXT = "#2";
 
+    private final SelenideElement SEARCH_LINE = $("input[name = 'q']"),
+            ISSUE_TAB = $("#issues-tab");
+
     @Test
-    @DisplayName("Issue search")
-    public void issueSearchTest() {
+    @DisplayName("Issue search - Selenide")
+    public void issueSearchSelenideTest() {
 
         SelenideLogger.addListener("allure", new AllureSelenide());
 
@@ -32,5 +37,34 @@ public class GithubTests extends BaseTest {
         $(linkText(REPOSITORY_PATH)).click();
         $("#issues-tab").click();
         $(withText(ISSUE_TEXT)).should(Condition.exist);
+    }
+
+    @Test
+    @DisplayName("Issue search - Lambda")
+    public void issueSearchLambdaTest() {
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открыть главную страницу " + BASE_URL, () -> {
+            open(BASE_URL);
+        });
+
+        step("Найти репозиторий " + REPOSITORY_PATH, () -> {
+            SEARCH_LINE.click();
+            SEARCH_LINE.setValue(REPOSITORY_PATH);
+            SEARCH_LINE.pressEnter();
+        });
+
+        step("Кликнуть по ссылке репозитория " + REPOSITORY_PATH, () -> {
+            $(linkText(REPOSITORY_PATH)).click();
+        });
+
+        step("Кликнуть по табу Issues", () -> {
+            ISSUE_TAB.click();
+        });
+
+        step("Проверить наличие Issue с номером " + ISSUE_TEXT, () -> {
+            $(withText(ISSUE_TEXT)).should(Condition.exist);
+        });
     }
 }
